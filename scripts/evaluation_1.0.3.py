68e6728922bc28392690b2afb1fc35dd0706980d
Markus Stoll <markus.stoll@renumics.com>
2023-12-10 17:21:12 +0100

    scripts back to format and lint

--- scripts/evaluation_1.0.3.py
+++ scripts/evaluation_1.0.3.py
@@ -1,17 +1,20 @@
-from mesh2vec.mesh2vec_cae import Mesh2VecCae
+"""
+measured time and memory usage of mesh2vec 1.0.3
+"""
+
 from pathlib import Path
 import time
-import numpy as np
 import tracemalloc
+import numpy as np
+from mesh2vec.mesh2vec_cae import Mesh2VecCae
+
 
 DIST = 30
 PATH = "data/hat/cached_hat_key.json"
-PATH = "/home/markus/Downloads/test_data_internal/p210.key_tmp.json"
 
-
-res = None
 PROFILE_MEM = True
 
+# pylint: disable=f-string-without-interpolation
 ### create neighbors
 start = time.time()
 if PROFILE_MEM:
@@ -83,7 +86,7 @@ if PROFILE_MEM:
     print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
     tracemalloc.stop()
 
-
+## tested with p210.key_tmp and mesh2vec 1.0.3
 # calc_adjacencies: 41.01193881034851
 # add_features_from_ansa: 0.21686553955078125
 # 1 aggregate: 27.74431562423706
